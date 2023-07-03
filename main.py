b867a0911216f60a09e288fe035e2a614cb66f22
efe.demir <efe.demir@eliarge.com>
2023-07-03 11:06:06 +0300

    dongu sayisi getirildi

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import randomdatamatching as RandomDataMatching
 import factorycircle as FactoryCircle
 import databasesave as dbsave
 
+count = 0
 
 def main():
     fabrics_in_the_factory, yabbys = RandomDataMatching.t_zero_monent()
-    while(True):
+    while count <= 10:
         fabrics_in_the_factory, yabbys = FactoryCircle.simulation_function(fabrics_in_the_factory, yabbys)
         # dbsave.dbsave(fabrics_in_the_factory)
+        count += 1
 
 if __name__ == "__main__":
     main()
